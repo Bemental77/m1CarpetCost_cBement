f500e1c6f7a66bc1215e2ceaaee978565db13e01
Bemental77 <bementcw@gmail.com>
2022-04-04 23:22:05 -0500

    updating totalCost project

--- totalCost_cBement.php
+++ totalCost_cBement.php
@@ -1,62 +1,76 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Total Cost!</title>
-		
-		<link href="css/styles.css" rel="stylesheet">
-    </head>
-    <body><?php
+<html lang="en">
 
-    function sanitizeString($field) {
-        return filter_input(INPUT_POST, $field, FILTER_SANITIZE_STRING);
-    }
+<head>
+    <title>Form that calls the totalCost.php program</title>
+    <meta charset="utf-8">
 
-    function sanitizeFloat($field){
-        return filter_input(INPUT_POST, $field, FILTER_SANITIZE_NUMBER_FLOAT);
-    }
+    <link href="css/styles.css" rel="stylesheet">
 
-    
-function totalCost($costPerWidget, $taxRate, $discountRate){
-  //set variables
-  $quantityWidgets = sanitizeFloat('quantity');
-  $costPerWidget = 20.00;
-  $taxRate = 1.06;
-  $discountRate = .75;
-  $totalCost = 0;
+</head>
 
-  $totalCost = $quantityWidgets * $costPerWidget
+<body>
+<?php
 
-    if($totalCost <= 0){
-      echo "Please make sure that you have entered a quantity and then resubmit";
-    }
-
-    if($totalCost >= 50){
-      $totalCost = ($totalCost * $discountRate) * $taxRate;
-    } else {
-      $totalCost = $totalCost * $taxRate;
-    }
-
+function sanitizeString($field) {
+    return filter_input(INPUT_POST, $field, FILTER_SANITIZE_STRING);
 }
 
+function sanitizeFloat($field){
+    return filter_input(INPUT_POST, $field, FILTER_SANITIZE_NUMBER_FLOAT);
+}
 function monthlyRate ($totalCost){
-  //monthly rate based on 12 month period
-  $monthlyRate = 0;
+    //monthly rate based on 12 month period
+    $monthlyRate = 0;
 
-  $monthlyRate = $totalCost / 12;
+    $monthlyRate = $totalCost / 12;
 
-  return number_format($monthlyRate, 2);
+    return number_format($monthlyRate, 2);
 }
+
+$submitPressed = sanitizeString('submit');
+if(isset($submitPressed)) {
+
+    function totalCost($costPerWidget, $taxRate, $discountRate)
+    {
+        //set variables
+        $quantityWidgets = sanitizeFloat('quantity');
+        $costPerWidget = 20.00;
+        $taxRate = 1.06;
+        $discountRate = .75;
+        $totalCost = 0;
+
+        $totalCost = $quantityWidgets * $costPerWidget;
+
+        if ($totalCost <= 0) {
+            echo "Please make sure that you have entered a quantity and then resubmit";
+        }
+
+        if ($totalCost >= 50) {
+            $totalCost = ($totalCost * $discountRate) * $taxRate;
+        } else {
+            $totalCost = $totalCost * $taxRate;
+        }
+
+        echo "You requested $quantityWidgets widget(s) at $20 each. Your total with tax, minus your $discountRate
+ discount, comes to $totalCost. You may purchase the widget(s) in 12 monthly installments of $ monthlyRate";
+
+    }
+    totalCost(20,1.06, .75);
+}
+
 
 function formatCost ($totalCost){
-  return number_format($totalCost, 2);
+    return number_format($totalCost, 2);
 }
 
-echo "You requested $quantityWidgets widget(s) at $20 each. Your total with tax, minus your $discountRate
- discount, comes to $totalCost. You may purchase the widget(s) in 12 monthly installments of $monthlyRate"
-    
-        
-    ?> 
-  </body>
+
+?>
+
+
+
+
+</body>
 </html>
+
         
